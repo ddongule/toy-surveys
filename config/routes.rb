de99811cd63669f1217452944b8ec310b24973d3
LDongGyu <ehdrbchd@naver.com>
2019-01-23 11:13:09 +0900

    warning route 설정

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   get 'cock_recommend/warning_update'
 
+  get 'cock_recommend/cocktail/warning_update'
+
   get 'cock_recommend/avoid'
 
   get 'cock_recommend/avoid_update'
