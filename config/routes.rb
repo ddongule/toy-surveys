c8f25708432ee7c350093fe2317858440f90d4db
LDongGyu <ehdrbchd@naver.com>
2019-01-29 19:49:36 +0900

    새로고침 방지

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,6 @@ Rails.application.routes.draw do
 
   get 'cock_recommend/cocktail/warning_update'
 
-  get 'cocktail/warning_update'
-
   get 'cock_recommend/avoid'
 
   get 'cock_recommend/avoid_update'
